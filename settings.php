3a440d90e57434d16099caaf95aaa5577f163451
nickstrelets <61534518+nickstrelets@users.noreply.github.com>
2022-01-21 15:15:07 +0200

    Add files via upload

---
+++ settings.php
@@ -0,0 +1,11 @@
+<?php
+//define('C_REST_CLIENT_ID','local.61eaaddb644e69.51441394');//Application ID
+//define('C_REST_CLIENT_SECRET','eqAz1jzSLLzd33u7Daw8r7o0wuoUCgK1w41QDFjOB7xyFugxqj');//Application key
+// or
+//define('C_REST_WEB_HOOK_URL','https://rest-api.bitrix24.com/rest/1/doutwqkjxgc3mgc1/');//url on creat Webhook
+
+//define('C_REST_CURRENT_ENCODING','windows-1251');
+//define('C_REST_IGNORE_SSL',true);//turn off validate ssl by curl
+//define('C_REST_LOG_TYPE_DUMP',true); //logs save var_export for viewing convenience
+//define('C_REST_BLOCK_LOG',true);//turn off default logs
+//define('C_REST_LOGS_DIR', __DIR__ .'/logs/'); //directory path to save the log
